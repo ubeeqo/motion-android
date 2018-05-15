09f824269c551d2e82e9c9ea54b1e5d132f23d6e
Osmany <osmany.rodriguez101@gmail.com>
2018-05-15 12:28:57 +0200

    Creating the DemoActivity and activity_demo_app, first launch work fine.
    Creating a model called ComponentModel in order to manage all component like and object

---
+++ app/src/main/java/model/ComponetModel.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ComponetModel {
+
+    private String name;
+
+    public ComponetModel(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+}
