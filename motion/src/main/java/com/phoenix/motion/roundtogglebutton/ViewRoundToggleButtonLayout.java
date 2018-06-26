94ff961d267806ee457a494dc7946883e432964f
Osmany <osmany.rodriguez101@gmail.com>
2018-06-26 16:54:26 +0200

    first approach to get attrs in ViewRoundToggleButton

--- motion/src/main/java/com/phoenix/motion/roundtogglebutton/ViewRoundToggleButtonLayout.java
+++ motion/src/main/java/com/phoenix/motion/roundtogglebutton/ViewRoundToggleButtonLayout.java
@@ -1,6 +1,8 @@
 package com.phoenix.motion.roundtogglebutton;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
@@ -14,16 +16,31 @@ public class ViewRoundToggleButtonLayout extends RelativeLayout {
     private TextView mTvTitle;
     private ImageView mIvToggleButton;
 
+    private int image;
+    private String title;
+
     public ViewRoundToggleButtonLayout(Context context) {
 
         super(context);
         initLayout();
     }
 
-    public ViewRoundToggleButtonLayout(Context context, AttributeSet attrs) {
+    public ViewRoundToggleButtonLayout(Context context, @Nullable AttributeSet attrs) {
 
         super(context, attrs);
         initLayout();
+
+        TypedArray arrayResurce = context.getTheme()
+                .obtainStyledAttributes(attrs,R.styleable.ViewRoundToggleButtonLayout,0,0);
+
+        try {
+
+            image = arrayResurce.getInteger(R.styleable.ViewRoundToggleButtonLayout_image,R.drawable.ic_add);
+            title = arrayResurce.getString(R.styleable.ViewRoundToggleButtonLayout_title);
+        } finally {
+
+            arrayResurce.recycle();
+        }
     }
 
     private void initLayout() {
@@ -39,6 +56,13 @@ public class ViewRoundToggleButtonLayout extends RelativeLayout {
 
         mTvTitle = findViewById(R.id.tv_toggle_button);
         mIvToggleButton = findViewById(R.id.iv_toggle_button);
+        fillFields();
+    }
+
+    private void fillFields() {
+
+        mTvTitle.setText(title);
+        mIvToggleButton.setImageResource(image);
     }
 
     public void setTitle(int title) {
