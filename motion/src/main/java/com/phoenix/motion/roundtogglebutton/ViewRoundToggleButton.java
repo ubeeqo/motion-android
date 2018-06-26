3d83f847aa558c1152804a35231a8eee095807bf
Osmany <osmany.rodriguez101@gmail.com>
2018-06-26 13:02:45 +0200

    Round toggle button implemented in motion

---
+++ motion/src/main/java/com/phoenix/motion/roundtogglebutton/ViewRoundToggleButton.java
@@ -0,0 +1,58 @@
+package com.phoenix.motion.roundtogglebutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.motion.R;
+
+public class ViewRoundToggleButton extends RelativeLayout {
+
+    private TextView mTvTitle;
+    private ImageView mIvToggleButton;
+
+    public ViewRoundToggleButton(Context context) {
+
+        super(context);
+        initLayout();
+    }
+
+    public ViewRoundToggleButton(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        initLayout();
+    }
+
+    private void initLayout() {
+
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_round_toggle_button, this, true);
+
+        bindViews();
+    }
+
+    private void bindViews() {
+
+        mTvTitle = findViewById(R.id.tv_toggle_button);
+        mIvToggleButton = findViewById(R.id.iv_toggle_button);
+    }
+
+    public void setTitle(int title) {
+
+        mTvTitle.setText(title);
+    }
+
+    public void setTitle(String title) {
+
+        mTvTitle.setText(title);
+    }
+
+    public void setImage(int image) {
+
+        mIvToggleButton.setImageResource(image);
+    }
+}
