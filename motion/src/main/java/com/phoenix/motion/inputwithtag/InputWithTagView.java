ec974f176fcba444cbb38b450ee0d0a17dd3a6dc
Toni <toni.roldan@ubeeqo.com>
2018-07-05 10:17:25 +0200

    New attributes on InputWithTagView "header_text" "input_text" "input_hint" "android:inputType"

--- motion/src/main/java/com/phoenix/motion/inputwithtag/InputWithTagView.java
+++ motion/src/main/java/com/phoenix/motion/inputwithtag/InputWithTagView.java
@@ -1,8 +1,11 @@
 package com.phoenix.motion.inputwithtag;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +17,11 @@ public class InputWithTagView extends LinearLayout {
     //region View Binding
     private TextView mTvHeader;
     private EditText mEtInput;
+
+    private String mHeader;
+    private String mInput;
+    private String mInputHint;
+    private Integer mInputType;
     //endregion
 
     //region Constructors and initialization
@@ -26,6 +34,7 @@ public class InputWithTagView extends LinearLayout {
                             AttributeSet attrs) {
 
         super(context, attrs);
+        setAttrs(context, attrs);
         inflateView();
     }
 
@@ -36,19 +45,56 @@ public class InputWithTagView extends LinearLayout {
         super(context, attrs, defStyleAttr);
     }
 
+    private void setAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.InputWithTagView,
+                0,
+                0);
+        try {
+
+            mHeader = a.getString(R.styleable.InputWithTagView_header_text);
+            mInput = a.getString(R.styleable.InputWithTagView_input_text);
+            mInputHint = a.getString(R.styleable.InputWithTagView_input_hint);
+            mInputType = a.getInt(R.styleable.InputWithTagView_android_inputType, EditorInfo.TYPE_TEXT_VARIATION_NORMAL);
+
+        } finally {
+            a.recycle();
+        }
+    }
+
     private void inflateView() {
 
         LayoutInflater inflater = LayoutInflater.from(getContext());
         inflater.inflate(R.layout.view_input_with_tag, this, true);
         bindViews();
     }
-    //endregion
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        bindViews();
+        populate();
+    }
 
     private void bindViews() {
 
         mTvHeader = findViewById(R.id.tv_header);
         mEtInput = findViewById(R.id.et_input);
     }
+
+    private void populate() {
+
+        mTvHeader.setText(mHeader);
+        mEtInput.setText(mInput);
+        mEtInput.setHint(mInputHint);
+
+        if (mInputType != 0) {
+            mEtInput.setInputType(mInputType);
+        }
+    }
+    //endregion
 
     public void configureView(String header,
                               String hint) {
