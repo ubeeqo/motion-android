b7ca33797268b1c45cf8476e7cc6f1cb47144c42
carlosblanco <carlos.blanco@ubeeqo.com>
2018-09-20 15:31:44 +0200

    change style test

--- motion/src/main/java/com/phoenix/motion/rows/RowValueLayout.java
+++ motion/src/main/java/com/phoenix/motion/rows/RowValueLayout.java
@@ -54,8 +54,8 @@ public class RowValueLayout extends LinearLayout {
 
             mHeaderString = a.getString(R.styleable.RowValueLayout_header_text);
             mTitleString = a.getString(R.styleable.RowValueLayout_title_text);
-            mHeaderStyle = a.getResourceId(R.styleable.RowValueLayout_header_style, R.styleable.RowValueLayout_header_style);
-            mTitleStyle = a.getResourceId(R.styleable.RowValueLayout_title_style, R.styleable.RowValueLayout_title_style);
+            mHeaderStyle = a.getResourceId(R.styleable.RowValueLayout_header_style, 0);
+            mTitleStyle = a.getResourceId(R.styleable.RowValueLayout_title_style, 0);
 
         } finally {
             a.recycle();
