56f0c5f01aa90a0f021a778e00b7e2e2c8c0a1a3
Osmany <osmany.rodriguez101@gmail.com>
2018-09-26 14:13:37 +0100

    fixing onclick in AlertBox and DangerBox (app-motion)

--- app/src/main/java/activities/alertbox/AlertBoxActivity.java
+++ app/src/main/java/activities/alertbox/AlertBoxActivity.java
@@ -6,12 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.phoenix.R;
 import com.phoenix.motion.alertbox.AlertBoxView;
+import com.phoenix.motion.alertbox.OnAlertBoxClickListener;
 import com.phoenix.motion.dangerbox.DangerBoxView;
+import com.phoenix.motion.dangerbox.OnDangerBoxClickListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AlertBoxActivity extends AppCompatActivity {
+public class AlertBoxActivity extends AppCompatActivity
+        implements OnAlertBoxClickListener,
+        OnDangerBoxClickListener {
 
     //region BindViews
     @BindView(R.id.alert_box_view_demo)
@@ -34,7 +38,14 @@ public class AlertBoxActivity extends AppCompatActivity {
 
         mAlertBoxView.setMessage("You have to complete missing documentation");
         mAlertBoxView.setAction("Let\'s do it now");
+        mAlertBoxView.setListener(this);
         mDangerBoxView.setMessage("We are still validating your license");
         mDangerBoxView.setAction("Complete it now");
+        mDangerBoxView.setListener(this);
+    }
+
+    @Override
+    public void onActionClick() {
+
     }
 }
